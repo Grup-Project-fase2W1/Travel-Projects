b740b9d19e18fc9056e314aa2cfa327ca5bcb4c0
ridwan11dvk <ridwan11depok@gmail.com>
2020-10-02 01:26:36 +0700

    Menambahkan 3rd API weatherstack

--- server/router/article.js
+++ server/router/article.js
@@ -2,7 +2,7 @@ const route = require("express").Router()
 const ArticleController = require("../controllers/articleController")
 
 route.get("/article", ArticleController.getDataZomato)
-route.get("/weater", ArticleController.weater)
+route.get("/weather", ArticleController.weather)
 
 
 module.exports = route
