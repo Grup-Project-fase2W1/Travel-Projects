a9e4e1b8fbba409289991272c6229cb0ef8e77c8
ridwan11dvk <ridwan11depok@gmail.com>
2020-10-02 12:39:31 +0700

    Menambahkan 3rd API ketiga, holidayapi, Membuat client side holidayapi API,wheatherstack API

--- server/router/article.js
+++ server/router/article.js
@@ -5,6 +5,7 @@ const UserController = require("../controllers/userController")
 route.get("/article", ArticleController.getDataZomato)
 route.post("/googleLogin", UserController.googleLogin)
 route.get("/weather", ArticleController.weather)
+route.get("/holiday", ArticleController.holiday)
 
 
 module.exports = route
